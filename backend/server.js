d97878daf39e1a6f715d6bf04bd1fd17af42ebd8
Maneesh Amera <maneesh.amera@advertalyst.ai>
2025-03-31 00:22:49 +0530

    updated depolyment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,13 @@ const itemsRouter = require('./routes/items');
 const app = express();
 require('dotenv').config();
 
-app.use(cors());
+app.use(
+    cors({
+      origin: "https://myprojectfrontend-beryl.vercel.app",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 app.use(express.json());
 app.use('/api/items', itemsRouter);
 
